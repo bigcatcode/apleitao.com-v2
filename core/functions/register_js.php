696cb877aa864472090e85c96c257a50251fb869
afonsot <afonso@getskybound.com>
2025-10-16 15:18:55 +0200

    update url PROD

--- core/functions/register_js.php
+++ core/functions/register_js.php
@@ -27,7 +27,7 @@ function custom_child_scripts() {
 
 		wp_enqueue_script(
 		    'projeto_virtual_js',
-		    CORE_URL . '/projeto-virtual/build/static/js/main.0b445236.js',
+		    CORE_URL . '/projeto-virtual/build/static/js/main.1200c941.js',
 	        array('jquery'), 
 	        rand(),
 	        true  
